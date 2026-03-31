9209ed7135d3127566fd2e10324c898fe290cabc
Fioneo <fioneo.dev@gmail.com>
2026-03-31 16:54:02 +0300

    feat(tasks): integrate task feature and set UTC timezone

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -6,18 +6,27 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	core_logger "github.com/Fioneo/taskflow/internal/core/logger"
 	core_postgres_pool "github.com/Fioneo/taskflow/internal/core/repository/pool"
 	core_http_middleware "github.com/Fioneo/taskflow/internal/core/transport/http/middleware"
 	core_http_server "github.com/Fioneo/taskflow/internal/core/transport/http/server"
+	tasks_repository "github.com/Fioneo/taskflow/internal/features/tasks/repository"
+	tasks_service "github.com/Fioneo/taskflow/internal/features/tasks/service"
+	tasks_transport "github.com/Fioneo/taskflow/internal/features/tasks/transport"
 	users_repository "github.com/Fioneo/taskflow/internal/features/users/repository"
 	users_service "github.com/Fioneo/taskflow/internal/features/users/service"
 	users_transport "github.com/Fioneo/taskflow/internal/features/users/transport"
 	"go.uber.org/zap"
 )
 
+var (
+	timeZone = time.UTC
+)
+
 func main() {
+	time.Local = timeZone
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
 		syscall.SIGINT,
@@ -32,6 +41,8 @@ func main() {
 	}
 	defer logger.Close()
 
+	logger.Debug("Time zone", zap.Any("zone", timeZone))
+
 	logger.Debug("Initializing postgres pool connection")
 
 	dbpool, err := core_postgres_pool.NewConnectionPool(core_postgres_pool.NewConfigMust(), ctx)
@@ -43,9 +54,15 @@ func main() {
 
 	logger.Debug("Initializing feature", zap.String("feature", "users"))
 
-	usersRepo := users_repository.NewUserRepostory(dbpool)
+	usersRepo := users_repository.NewUsersRepostory(dbpool)
 	usersService := users_service.NewUsersService(usersRepo)
 	usersTransportHTTP := users_transport.NewUsersHTTPHandler(usersService)
+
+	logger.Debug("Initializing feature", zap.String("feature", "tasks"))
+
+	tasksRepo := tasks_repository.NewTasksRepository(dbpool)
+	tasksService := tasks_service.NewTasksService(tasksRepo)
+	tasksTransportHTTP := tasks_transport.NewTasksHTTPHandler(tasksService)
 
 	logger.Debug("Initializing HTTP server")
 
@@ -60,6 +77,8 @@ func main() {
 
 	apiVersionRouter := core_http_server.NewApiVersionRouter(core_http_server.ApiVersion1)
 	apiVersionRouter.RegisterRoutes(usersTransportHTTP.Routes()...)
+	apiVersionRouter.RegisterRoutes(tasksTransportHTTP.Routes()...)
+
 	httpServer.RegisterApiRoutes(apiVersionRouter)
 
 	if err := httpServer.Run(ctx); err != nil {
